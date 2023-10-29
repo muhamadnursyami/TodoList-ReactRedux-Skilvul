2d951f2c534e7a23a9779772685894a595877f51
Muhamad Nur Syami <nursami385@gmail.com>
2023-10-29 23:16:11 +0700

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+
+  return (
+    <>
+    <div className="container mx-auto my-14 text-center">
+        <h2 className="text-3xl  font-bold ">
+      Whats the plan for today?
+        </h2>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
